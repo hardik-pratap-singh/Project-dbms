227878c49ffd3fc8049017b4ca3986f592d10f30
Hardik <hardikjanuary2021@gmail.com>
2022-10-15 13:18:35 +0530

    hakd

--- src/App.js
+++ src/App.js
@@ -7,11 +7,6 @@ function App() {
     <div>
      <Navbar />
 
-     This line is for learning. Nothing spl. 
-     I wrote this in master branch
-     This is a line in newbranch
-
-     Changes done in the master branch. 
     </div>
   );
 }
