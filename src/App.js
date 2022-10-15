2fdf5421e55f961ec713525ab871cab6688c2049
Hardik <hardikjanuary2021@gmail.com>
2022-10-15 21:50:31 +0530

    Added login and signup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react'
-import Navbar from './components/Navbar';
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
+      <Router>
     <div>
-     <Navbar />
-
-    This is new File. 
+        <Navbar />
+        <Routes>
+          <Route path="/Login" element={<Login/>}/>
+          <Route path="/Signup" element={<Signup />} />
+        </Routes>
     </div>
+      </Router>
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "../styles/login.css";
+// import Signup from "./Signup";
+
+
+function Login() {
+  return (
+    <>
+
+    
+      <div className="mainhead">
+        <div className="header">
+          <h1>LOGIN HERE</h1>
+        </div>
+        <div className="login-page">
+          <div className="form">
+            <form method="post" action="loginServlet">
+              <input type="text" name="name" placeholder="User Name" required />
+              <br />
+              <br />
+              <input type="password" name="password" placeholder="Password" />
+              <br />
+              <br />
+              <input id="btn" type="submit" value="Login" />
+              <br />
+              <p>
+                Don't have an account ?
+                <a href="./Signup">
+                  {/* <br /> */}
+                  Register now
+                </a>
+              </p>
+              <br />
+              <p>
+                <a href="forgot.html">Forgot Password ?</a>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Login;
