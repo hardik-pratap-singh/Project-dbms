2f3fd995b476ff1dbbe72a603a6cd41d10b3bcb9
Hardik <hardik182002@gmail.com>
2022-10-15 18:40:58 +0530

    Added items and Sticky Navbar

---
+++ src/components/FreshItems.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Item from './Item';
+
+function FreshItems() {
+    return (
+        <div className='container'>
+            <h2>Fresh recommendations</h2>
+
+            <div className='row'>
+                <div className='col-md-4'>
+                    <Item title="Phones" description="iPhone" imgUrl="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-model-unselect-gallery-2-202209_GEO_EMEA?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1660753617539" />
+                </div>
+                <div className='col-md-4'>
+                    <Item title="Laptop" description="Best" imgUrl="https://m.media-amazon.com/images/I/718ETwvLVOL._SX425_.jpg" />
+                </div>
+                <div className='col-md-4'>
+                    <Item title="Phones" description="iPhone" imgUrl="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-model-unselect-gallery-2-202209_GEO_EMEA?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1660753617539" />
+                </div>
+                <div className='col-md-4'>
+                    <Item title="Phones" description="iPhone" imgUrl="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-model-unselect-gallery-2-202209_GEO_EMEA?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1660753617539" />
+                </div>
+                <div className='col-md-4'>
+                    <Item title="Phones" description="iPhone" imgUrl="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-model-unselect-gallery-2-202209_GEO_EMEA?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1660753617539" />
+                </div>
+                <div className='col-md-4'>
+                    <Item title="Phones" description="iPhone" imgUrl="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-model-unselect-gallery-2-202209_GEO_EMEA?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1660753617539" />
+                </div>
+                <div className='col-md-4'>
+                    <Item title="Phones" description="iPhone" imgUrl="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-model-unselect-gallery-2-202209_GEO_EMEA?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1660753617539" />
+                </div>
+                <div className='col-md-4'>
+                    <Item title="Phones" description="iPhone" imgUrl="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-model-unselect-gallery-2-202209_GEO_EMEA?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1660753617539" />
+                </div>
+                <div className='col-md-4'>
+                    <Item title="Phones" description="iPhone" imgUrl="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-model-unselect-gallery-2-202209_GEO_EMEA?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1660753617539" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default FreshItems;
