799d54a6990d24b32fced769051f3482a6c3d25a
Hardik <hardik182002@gmail.com>
2022-10-15 16:28:45 +0530

    Added Sell Button

---
+++ src/components/Sell.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Container, Button } from 'react-floating-action-button'
+
+function Sell() {
+    return (
+        <Container>
+            <Button
+                className="fab-item btn btn-link btn-lg text-white sell-btn"
+                tooltip="Sell you items"
+                rotate={true}
+                // onClick={() => alert('FAB Rocks!')}
+                
+                >Sell</Button>
+        </Container>
+    )
+}
+
+export default Sell;
